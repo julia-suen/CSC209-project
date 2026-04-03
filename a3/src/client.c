64dc35077b6e322a052357658f32032ea03e22e1
Suen Hau Yung <suenhau@teach.cs.toronto.ca>
2026-04-03 07:51:27 -0400

    added basic client connection

---
+++ a3/src/client.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>    /* Internet domain header */
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main() {
+    // create socket
+    int soc = socket(AF_INET, SOCK_STREAM, 0);
+    if (soc == -1) {
+        perror("client: socket");
+        exit(1);
+    }
+
+    //initialize server address    
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(58086);  
+    memset(&server.sin_zero, 0, 8);
+    
+    struct addrinfo *ai;
+    char * hostname = "teach.cs.toronto.edu";
+
+    /* this call declares memory and populates ailist */
+    getaddrinfo(hostname, NULL, NULL, &ai);
+    /* we only make use of the first element in the list */
+    server.sin_addr = ((struct sockaddr_in *) ai->ai_addr)->sin_addr;
+
+    // free the memory that was allocated by getaddrinfo for this list
+    freeaddrinfo(ai);
+
+    int ret = connect(soc, (struct sockaddr *)&server, sizeof(struct sockaddr_in));
+
+    printf("Connect returned %d\n", ret);
+    return 0;
+}
+
