cf4ffa3f41fc81629b6ef1b19becc52f0fde6a1e
Kira Sandhu <kira.sandhu@mail.utoronto.ca>
2026-04-03 23:42:28 -0400

    fix commands parsing

--- a3/src/commands.c
+++ a3/src/commands.c
@@ -1,69 +1,72 @@
-#include "../include/command.h"
+#include "../include/commands.h"
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-static void trim_newline(char *s){
+static void trim_newline(char *s) {
     size_t len;
 
-    if (s == NULL){
+    if (s == NULL) {
         return;
     }
+
     len = strlen(s);
 
-    while (len > 0 && (s[len - 1] == '\n' || s[len - 1] == '\r')){
+    while (len > 0 && (s[len - 1] == '\n' || s[len - 1] == '\r')) {
         s[len - 1] = '\0';
         len--;
     }
 }
 
-static void skip_spaces(const char **p){
+static void skip_spaces(const char **p) {
     while (**p != '\0' && isspace((unsigned char)**p)) {
         (*p)++;
     }
 }
 
-void init_command(Command *cmd){
-    if (cmd == NULL){
+void init_command(Command *cmd) {
+    if (cmd == NULL) {
         return;
     }
+
     memset(cmd, 0, sizeof(Command));
     cmd->type = CMD_INVALID;
 }
 
-int parse_command(const char *input, Command *cmd){
+int parse_command(const char *input, Command *cmd) {
     char buffer[MAX_MSG];
     const char *p;
     char *space;
 
-    if (input == NULL || cmd == NULL){
+    if (input == NULL || cmd == NULL) {
         return -1;
     }
+
     init_command(cmd);
 
-    strncpy(buffer, input, MAX_MSG -1);
+    strncpy(buffer, input, MAX_MSG - 1);
     buffer[MAX_MSG - 1] = '\0';
     trim_newline(buffer);
 
     p = buffer;
     skip_spaces(&p);
 
-    if (*p == '\0'){
+    if (*p == '\0') {
         cmd->type = CMD_INVALID;
         return -1;
     }
 
-     /* normal text message */
-    if (*p != '/'){
+    /* normal text message */
+    if (*p != '/') {
         cmd->type = CMD_TEXT;
-        strncpy(cmd->arg2, p, MAX_MSG -1);
+        strncpy(cmd->arg2, p, MAX_MSG - 1);
         cmd->arg2[MAX_MSG - 1] = '\0';
         return 0;
     }
 
-}
- if (strncmp(p, "/nick", 5) == 0 && (p[5] == '\0' || isspace((unsigned char)p[5]))) {
+    if (strncmp(p, "/nick", 5) == 0 &&
+        (p[5] == '\0' || isspace((unsigned char)p[5]))) {
         p += 5;
         skip_spaces(&p);
 
@@ -78,7 +81,8 @@ int parse_command(const char *input, Command *cmd){
         return 0;
     }
 
-    if (strncmp(p, "/join", 5) == 0 && (p[5] == '\0' || isspace((unsigned char)p[5]))) {
+    if (strncmp(p, "/join", 5) == 0 &&
+        (p[5] == '\0' || isspace((unsigned char)p[5]))) {
         p += 5;
         skip_spaces(&p);
 
@@ -93,7 +97,8 @@ int parse_command(const char *input, Command *cmd){
         return 0;
     }
 
-    if (strncmp(p, "/leave", 6) == 0 && (p[6] == '\0' || isspace((unsigned char)p[6]))) {
+    if (strncmp(p, "/leave", 6) == 0 &&
+        (p[6] == '\0' || isspace((unsigned char)p[6]))) {
         p += 6;
         skip_spaces(&p);
 
@@ -107,7 +112,8 @@ int parse_command(const char *input, Command *cmd){
         return 0;
     }
 
-    if (strncmp(p, "/msg", 4) == 0 && (p[4] == '\0' || isspace((unsigned char)p[4]))) {
+    if (strncmp(p, "/msg", 4) == 0 &&
+        (p[4] == '\0' || isspace((unsigned char)p[4]))) {
         p += 4;
         skip_spaces(&p);
 
@@ -116,7 +122,7 @@ int parse_command(const char *input, Command *cmd){
             return -1;
         }
 
-        space = strchr(p, ' ');
+        space = strchr((char *)p, ' ');
         if (space == NULL) {
             cmd->type = CMD_INVALID;
             return -1;
