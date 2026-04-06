6b033f1571e0f551e73b244fae7b9f9781cb3ae1
julia-suen <134638508+julia-suen@users.noreply.github.com>
2026-04-06 15:40:59 -0400

    Revert "Server chatroom state rewrite"

--- a3/include/serverutil.h
+++ a3/include/serverutil.h
@@ -10,18 +10,18 @@
 
 int remove_client_from_list(usr_data *client_fds, usr_data client, server_data *server);
 pkt_node *add_to_pkt_list(int fd, pkt_node *pkts, Packet *pkt, server_data *server);
-pkt_node *remove_pkt_and_deallocate(pkt_node *pkts, pkt_node *pkt, server_data *server_data);
+pkt_node *remove_pkt_and_deallocate(pkt_node *pkts, pkt_node *pkt);
 // void get_client_by_id(int id, usr_node *list);
 // void get_client_by_fd(int fd, usr_node *list);
 // void get_client_by_name(char *name, usr_node *list);
 
 int process_dm(usr_data *clients, Packet *pkt, server_data *server);
 int process_text(chatroom *rooms, Packet *pkt, int num_rooms);
-int process_leave(chatroom *rooms, usr_data *clients, int fd, int num_clients);
-int process_join(usr_data* clients, int num_client, chatroom *rooms, Packet *pkt, int fd, int num_rooms);
+int process_leave();
+int process_join(chatroom *rooms, Packet *pkt, int fd, int num_rooms);
 int process_nick(usr_data *clients, int num_clients, int fd, Packet *pkt);
-int process_who(usr_data* clients, int fd, chatroom *rooms, server_data *server);
-int process_list(usr_data* clients, int fd, chatroom *rooms, server_data *server);
+int process_who();
+int process_list();
 int process_quit(chatroom *rooms, int num_rooms, int fd, Packet *pkt);
 
 #endif
